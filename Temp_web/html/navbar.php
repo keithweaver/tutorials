93c8f0798d8230be66b0a85a4fcd4066cff703d2
Keith Weaver <keithmweaver@gmail.com>
2015-03-14 15:02:14 -0400

    event site

---
+++ Temp_web/html/navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-default" id="navbar_menu">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+          <img src="./imgs/qcop_logo.png" id="navbar_logo"/>
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <!--
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    
+      <ul class="nav navbar-nav navbar-right">
+    -->
+        <!--
+        <li><a href="#">Link</a></li>
+        -->
+    <!--
+      </ul>
+    -->
+    <!--
+    </div>
+    -->
+    <!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
